acae6479a97e1172de184116641531169c4d168a
Nazzcodek <bellnas09@gmail.com>
2023-10-29 06:17:26 -0700

    added /

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,23 +7,23 @@ from flask import jsonify
 from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status/', strict_slashes=False)
 def status():
     """returns app_views API status"""
     return jsonify(status='Ok')
 
 
-@app_views.route('/stats', strict_slashes=False)
+@app_views.route('/stats/', strict_slashes=False)
 def stats():
     """This endpoint retrieves the number of each objects by type"""
     objects = {
-            "Amenity": "amenities",
-            "City": "cities",
-            "Place": "places",
-            "Review": "reviews",
-            "State": "states",
-            "User": "users"
-        }
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+    }
     response = {}
     for k, v in objects.items():
         response[v] = storage.count(k)
