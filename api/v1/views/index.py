1aadf6fd2f10d8dc587996d745ec97dcc481b9fc
Nazzcodek <bellnas09@gmail.com>
2023-10-29 06:37:38 -0700

    OK

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,7 +10,7 @@ from models import storage
 @app_views.route('/status/', strict_slashes=False)
 def status():
     """returns app_views API status"""
-    return jsonify(status='Ok')
+    return jsonify(status='OK')
 
 
 @app_views.route('/stats/', strict_slashes=False)
